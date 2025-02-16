ba0e1b19c106087f2fc9c9bce4aa2988dfddebed
Afrin <it23039@mbstu.ac.bd>
2025-02-16 10:36:26 +0600

    Add files via upload

---
+++ CircleMain.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Circle {
+    private double radius;
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("Radius cannot be negative.");
+        }
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        try {
+            Scanner sc=new Scanner(System.in);
+            double r = sc.nextDouble();
+            circle.setRadius(r); 
+            System.out.println("Area: " + circle.getArea());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+}
